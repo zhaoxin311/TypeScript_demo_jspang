da29a187265ede4843f0dc916d2dd17a27f2e655
zhaoxin0311 <1204090769@qq.com>
2021-09-25 17:48:23 +0800

    TypeScript 函数

---
+++ demo_3/demo.ts
@@ -0,0 +1,70 @@
+
+//把功能相近的需求 封装成一个代码块，每次传入不同的变量，变量不同 呈现的结果也有所不同
+//age 为一个形式参数  此处的string为返回函数类型
+function searchPeople(age:number):string  {
+  return '找到了'+age+'的小朋友'
+}
+
+var age:number=18
+var result:string=searchPeople(age)
+console.log(result);
+// 需要注意的是：
+
+// 1. 声明（定义）函数必须加 function 关键字；
+// 2. 函数名与变量名一样，命名规则按照标识符规则；
+// 3. 函数参数可有可无，多个参数之间用逗号隔开；
+// 4. 每个参数参数由名字与类型组成，之间用分号隔开；
+// 5. 函数的返回值可有可无，没有时，返回类型为 void；
+// 6. 大括号中是函数体。
+
+console.log('-----------------我是分割线-----------------');
+
+//有可选参数的函数
+//加个问号  代表了可选的形参
+console.log('1. 有可选参数的函数');
+function search1(age:number,stature?:string):string {
+  let yy:string=''
+  yy='找到'+age+'岁'
+  if(stature!=undefined){
+    yy=yy+stature
+  }
+  return yy+'小朋友'
+}
+var result:string=search1(22,'瘦瘦的')
+console.log(result);
+
+console.log('-----------------我是分割线-----------------');
+
+console.log('2. 有默认参数的函数');
+function search2(age:number=19,stature:string='高高的'):string {
+  let yy:string=''
+  yy='找到'+age+'岁'
+  if(stature!=undefined){
+    yy=yy+stature
+  }
+  return yy+'小朋友'
+}
+//调用函数时，不传实参时，会展示默认参数。
+var result:string=search2()
+console.log(result,'默认参数');
+//调用函数时，传递实参，结果会展示实参内容
+var result:string=search2()
+console.log(result,'传递的实参');
+
+
+console.log('-----------------我是分割线-----------------');
+
+console.log('3. 有剩余参数的函数');
+//有剩余参数的函数，传递给函数的参数个数不确定
+//说的技术点，剩余参数就是形参是一个数组，传递几个实参过来都可以直接存在形参的数组中。
+function search3(...xuqiu:string[]):string {
+  let yy:string=''
+  yy='找到'
+  for(let i=0;i<xuqiu.length;i++){
+    yy=yy+xuqiu[i]
+
+  }
+  return yy+'小朋友'
+}
+var result:string=search3('19岁','白的','高的','瘦瘦的','美美的')
+console.log(result);
